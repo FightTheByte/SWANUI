311a4ea7c4cf9d2bae5997d43fc9de44f9aabc18
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-02 22:20:59 +0000

    completed insertEvent test

---
+++ src/test/java/customevents/InsertEventTest.java
@@ -0,0 +1,21 @@
+package customevents;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class InsertEventTest {
+
+    InsertEvent insertEvent;
+
+    @BeforeEach
+    public void instantiate(){
+        insertEvent = new InsertEvent();
+    }
+
+    @Test
+    void insertEventTest(){
+        assertTrue(insertEvent instanceof InsertEvent, "insertEvent Object not instantiated");
+    }
+}
