d0dc60cf5324b108ad62fe38e7e738b935a41787
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-04 03:37:41 +0000

    finished FileHandler, passes test, fixed spelling error, added directories

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,6 @@ module org.example.cob {
 
     opens org.example.cob to javafx.fxml;
     opens org.example.cob.eventbus to com.google.common;
+    opens org.example.cob.handlers to com.google.common;
     exports org.example.cob;
 }
--- src/test/java/org/example/cob/handlers/FileHandlerTest.java
+++ src/test/java/org/example/cob/handlers/FileHandlerTest.java
@@ -2,51 +2,40 @@ package org.example.cob.handlers;
 
 import com.google.common.eventbus.EventBus;
 import org.example.cob.customevents.WriteToFileEvent;
-import org.example.cob.eventbus.SwanEventBus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.example.cob.handlers.FileHandler;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class FileHandlerTest {
 
-    FileHander fileHander;
+    FileHandler fileHandler;
     File file;
     EventBus eventBus;
 
     @BeforeEach
-    public void instantiate(){
+    public void instantiate() throws IOException {
         fileHandler = new FileHandler();
         eventBus = new EventBus();
     }
 
     @Test
-    void writeToFileTest(){
-        fileHander.writeToFile("this is a test");
-        file = new File("parameters.dat");
-        try(Scanner scanner = new Scanner(file)){
-            String data = scanner.nextLine();
-            assertEquals("this is a test", data, "FileHandler did not write to file correctly");
-        } catch(FileNotFoundException e){
-            fail("FileHandler did not write to file correctly");
-        }
-    }
-
-    @Test
     void subscribeAndPublishFileHandlerTest(){
-        eventBus.register(fileHander);
-        eventBus.post(new WriteToFileEvent("this is test2"));
+        eventBus.register(fileHandler);
+        eventBus.post(new WriteToFileEvent("this is a test2"));
         file = new File("parameters.dat");
         try(Scanner scanner = new Scanner(file)){
             String data = scanner.nextLine();
             assertEquals("this is a test2", data, "FileHandler did not write to file correctly");
         } catch(FileNotFoundException e){
-            fail("FileHandler did not write to file correctly");
+            fail(e);
         }
     }
 }
