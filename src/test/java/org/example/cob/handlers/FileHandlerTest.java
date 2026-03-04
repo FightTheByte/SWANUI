cabb439b8b9c5e0aa5b27b4e597ebef64ecff3af
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-04 02:55:13 +0000

    finished second test

--- src/test/java/org/example/cob/handlers/FileHandlerTest.java
+++ src/test/java/org/example/cob/handlers/FileHandlerTest.java
@@ -1,6 +1,7 @@
 package org.example.cob.handlers;
 
 import com.google.common.eventbus.EventBus;
+import org.example.cob.customevents.WriteToFileEvent;
 import org.example.cob.eventbus.SwanEventBus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,6 +39,14 @@ public class FileHandlerTest {
 
     @Test
     void subscribeAndPublishFileHandlerTest(){
-
+        eventBus.register(fileHander);
+        eventBus.post(new WriteToFileEvent("this is test2"));
+        file = new File("parameters.dat");
+        try(Scanner scanner = new Scanner(file)){
+            String data = scanner.nextLine();
+            assertEquals("this is a test2", data, "FileHandler did not write to file correctly");
+        } catch(FileNotFoundException e){
+            fail("FileHandler did not write to file correctly");
+        }
     }
 }
