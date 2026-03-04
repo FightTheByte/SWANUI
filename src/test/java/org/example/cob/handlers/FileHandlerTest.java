a882b43e6913989ee8f6bd29471b9b8beed974ad
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-04 02:12:46 +0000

    added first write to file test

---
+++ src/test/java/org/example/cob/handlers/FileHandlerTest.java
@@ -0,0 +1,43 @@
+package org.example.cob.handlers;
+
+import com.google.common.eventbus.EventBus;
+import org.example.cob.eventbus.SwanEventBus;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class FileHandlerTest {
+
+    FileHander fileHander;
+    File file;
+    EventBus eventBus;
+
+    @BeforeEach
+    public void instantiate(){
+        fileHandler = new FileHandler();
+        eventBus = new EventBus();
+    }
+
+    @Test
+    void writeToFileTest(){
+        fileHander.writeToFile("this is a test");
+        file = new File("parameters.dat");
+        try(Scanner scanner = new Scanner(file)){
+            String data = scanner.nextLine();
+            assertEquals("this is a test", data, "FileHandler did not write to file correctly");
+        } catch(FileNotFoundException e){
+            fail("FileHandler did not write to file correctly");
+        }
+    }
+
+    @Test
+    void subscribeAndPublishFileHandlerTest(){
+
+    }
+}
