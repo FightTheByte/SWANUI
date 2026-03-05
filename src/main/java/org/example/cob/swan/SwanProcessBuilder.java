a68880841b5f47b21d07c622009ac13964198ec2
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-05 00:49:30 +0000

    Completed SwanProcessBuilder class, fixed return type on test

--- src/main/java/org/example/cob/swan/SwanProcessBuilder.java
+++ src/main/java/org/example/cob/swan/SwanProcessBuilder.java
@@ -1,7 +1,21 @@
 package org.example.cob.swan;
 
+import java.io.IOException;
+import java.util.List;
+
 public class SwanProcessBuilder {
     ProcessBuilder processBuilder;
 
-
+    public String runModel(String[] commands){
+        try{
+            processBuilder = new ProcessBuilder(commands);
+            Process process = processBuilder.start();
+            process.waitFor();
+            String results = new String(process.getInputStream().readAllBytes());
+            System.out.println(results);
+            return results;
+        } catch(Exception e){
+            return e.toString();
+        }
+    }
 }
