5bf97772f1fa6e83c996192089e4b31f3ecc6406
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-05 18:32:36 +0000

    created Parameter class, passes test

--- src/test/java/org/example/cob/util/ParameterTest.java
+++ src/test/java/org/example/cob/util/ParameterTest.java
@@ -3,6 +3,7 @@ package org.example.cob.util;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+
 import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
