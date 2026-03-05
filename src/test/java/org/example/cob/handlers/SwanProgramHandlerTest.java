cd66ee8c1afa73639c0e43643d3bc06ad1ada5ce
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-05 03:30:57 +0000

    added test for swanProgramHandler

---
+++ src/test/java/org/example/cob/handlers/SwanProgramHandlerTest.java
@@ -0,0 +1,42 @@
+package org.example.cob.handlers;
+
+import com.google.common.eventbus.DeadEvent;
+import com.google.common.eventbus.Subscribe;
+import org.example.cob.customevents.ReturnSwanResultEvent;
+import org.example.cob.swan.SwanAdapter;
+import org.example.cob.swan.SwanAdapterTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import com.google.common.eventbus.EventBus;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SwanProgramHandlerTest {
+
+    String result;
+    EventBus eventBus;
+    SwanProgramHandler swanProgramHandler;
+
+    @BeforeEach
+    public void initialise(){
+        eventBus = new EventBus();
+        swanProgramHandler = new SwanProgramHandler();
+        eventBus.register(new StringListener());
+    }
+
+    class StringListener {
+        @Subscribe
+        public void resultEvent(ReturnSwanResultEvent results){result = results.returnSwanResult();}
+    }
+
+    @Test
+    void testRunSwan(){
+        swanProgramHandler.runSwan();
+        assertEquals("success", result, "swanProgramHandler did not emit and return correct String");
+    }
+
+
+
+
+
+}
