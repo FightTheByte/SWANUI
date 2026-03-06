8843f7bb8460936b615ca5fee23e5b8ed1825825
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-06 03:00:43 +0000

    finished ErrorEventTest

---
+++ src/test/java/org/example/cob/customevents/ErrorEventTest.java
@@ -0,0 +1,30 @@
+package org.example.cob.customevents;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ErrorEventTest {
+
+    ErrorEvent errorEvent;
+
+    @BeforeEach
+    public void instantiate(){
+        errorEvent = new ErrorEvent("error");
+    }
+
+    @Test
+    void createsInstanceTest(){
+        assertTrue(errorEvent instanceof ErrorEvent);
+    }
+
+    @Test
+    void getErrorTest(){
+        String error = errorEvent.getError();
+        assertTrue(Objects.equals(error, "error"));
+    }
+}
