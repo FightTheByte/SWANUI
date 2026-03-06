b6fcacc162e8e9b80bf28db9debf502f2b8cb5b1
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-06 03:59:04 +0000

    finished PoolHandler class

--- src/main/java/org/example/cob/handlers/PoolHandler.java
+++ src/main/java/org/example/cob/handlers/PoolHandler.java
@@ -1,23 +1,28 @@
 package org.example.cob.handlers;
 
+import org.example.cob.customevents.CustomEventUtitlity;
+import org.example.cob.customevents.ErrorEvent;
+import org.example.cob.customevents.ReturnSelectEvent;
+import org.example.cob.customevents.ReturnSwanResultEvent;
 import org.example.cob.database.DatabasePool;
 import org.example.cob.eventbus.SwanEventBus;
+import org.example.cob.util.Parameter;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class PoolHandler {
     DatabasePool db;
+    private List<Parameter> parameters;
 
     public PoolHandler(String database){
         db = new DatabasePool(database);
     }
 
     public void insert(String name, String parameters) {
-        if(name.length() < 251) {
+        if(name.length() < 61) {
             String query = "INSERT INTO parameters (name, params) VALUES (?,?);";
             try (
                     Connection conn = db.connection();
@@ -27,15 +32,40 @@ public class PoolHandler {
                 insertIntoDb.setString(2, parameters);
                 insertIntoDb.execute();
             } catch (SQLException e) {
-
+                     emitError(e.toString()) ;
             }
+        }
+        else {
+
+            emitError("Name too long");
         }
     }
 
     public void select() {
+        parameters = new ArrayList<>();
+        try(
+            Connection conn = db.connection();
+            Statement stat = conn.createStatement();
+        ) {
+            ResultSet results = stat.executeQuery("SELECT * FROM parameters;");
+            do{
+                parameters.add(new Parameter(results.getString("name"), results.getString("params")));
+                results.next();
+            }while(results.next());
+
+        }
+        catch(SQLException e){
+            emitError(e.toString());
+            return;
+        }
+        returnSelectResult(parameters);
     }
 
-    public void returnSelectResult(){
+    public void returnSelectResult(List<Parameter> results){
+        SwanEventBus.returnEventBus().post(new ReturnSelectEvent(results));
+    }
 
+    private void emitError(String error){
+        SwanEventBus.returnEventBus().post(new ErrorEvent(error));
     }
 }
