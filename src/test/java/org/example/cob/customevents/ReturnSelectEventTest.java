c0195a08e0a05d7f970e7fd270a37082fc872b25
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-06 00:43:32 +0000

    Fixed the ReturnSelectEvent class and test, now uses a List or Parameter types

--- src/test/java/org/example/cob/customevents/ReturnSelectEventTest.java
+++ src/test/java/org/example/cob/customevents/ReturnSelectEventTest.java
@@ -1,9 +1,13 @@
 package org.example.cob.customevents;
 
+import org.example.cob.util.Parameter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.json.JsonObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,11 +18,14 @@ public class ReturnSelectEventTest {
 
     @BeforeEach
     public void instantiate(){
-        returnSelectEvent = new ReturnSelectEvent("result");
+        List<Parameter> result = new ArrayList<>();
+        result.add(new Parameter("test-name", "test-param"));
+        returnSelectEvent = new ReturnSelectEvent(result);
     }
 
     @Test
-    public void returnSwanResultEventTest(){
-        assertEquals("result", returnSelectEvent.getSelectResult(), "Did not return expected String result");
+    public void returnSelectEventTest(){
+        assertEquals("test-name", returnSelectEvent.getSelectResult().getFirst().getName(), "Did not return expected Array, with parameter object, not providing the correct method and name value");
+        assertEquals("test-param", returnSelectEvent.getSelectResult().getFirst().getParameters(), "Did not return expected Array, with parameter object, not providing the correct method and param value");
     }
 }
--- src/test/java/org/example/cob/handlers/PoolHandlerTest.java
+++ src/test/java/org/example/cob/handlers/PoolHandlerTest.java
@@ -1,15 +1,16 @@
 package org.example.cob.handlers;
 
 import org.example.cob.database.DatabasePool;
+import org.example.cob.util.Parameter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.json.Json;
 import javax.json.JsonObject;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 
 /*public class PoolHandlerTest {
@@ -20,6 +21,16 @@ import java.sql.Statement;
     public void instantiate(){
         db = new DatabasePool("test.db");
         poolHandler = new PoolHandler("test.db");
+        try(
+                Connection conn = db.connection();
+                Statement stat = conn.createStatement();
+        )
+        {
+            stat.executeQuery("DROP * FROM parameters;");
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
     }
 
     @Test
@@ -37,20 +48,32 @@ import java.sql.Statement;
                 .build();
             String insertVals = json.toString();
             poolHandler.insert(insertVals);
-
+            ResultSet results = stat.executeQuery("SELECT name, params FROM parameters;");
+            Parameter parameter = new Parameter(results.getString("name"), results.getString("params"));
+            assertEquals("test-name", parameter.getName(), "name not inserted into database");
+            assertEquals("test-params", parameter.getParameters(), "params not inserted into database");
         }
         catch(SQLException e){
-
+            fail(e);
         }
     }
 
     @Test
     void selectsFromDatabase(){
+        try(
+                Connection conn = db.connection();
+                Statement stat = conn.createStatement();
+        )
+        {
+            stat.executeQuery("INSERT INTO parameters (name, params) VALUES ('test-name', 'test-params');");
+            poolHandler.select();
 
+            assertEquals("test-name", parameter.getName(), "name not inserted into database");
+
+        }
+        catch(SQLException e){
+            fail(e);
+        }
     }
 
-    @Test
-    void selectResultEventEmits(){
-
-    }
 }*/
