9b2a9cdcbb865e8f034c9e1380f77de52d9b989e
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-06 03:59:55 +0000

    fixed executeQuery error

--- src/test/java/org/example/cob/handlers/PoolHandlerTest.java
+++ src/test/java/org/example/cob/handlers/PoolHandlerTest.java
@@ -4,13 +4,13 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import org.example.cob.customevents.ErrorEvent;
 import org.example.cob.customevents.ReturnSelectEvent;
+import org.example.cob.customevents.ReturnSwanResultEvent;
 import org.example.cob.database.DatabasePool;
+import org.example.cob.eventbus.SwanEventBus;
 import org.example.cob.util.Parameter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.json.Json;
-import javax.json.JsonObject;
 import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,15 +19,15 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PoolHandlerTest {
     DatabasePool db;
     PoolHandler poolHandler;
-    EventBus eventBus;
     String name;
     String params;
     String error;
 
     @BeforeEach
     public void instantiate(){
-        eventBus = new EventBus();
-        eventBus.register(new Listener());
+
+        SwanEventBus.returnEventBus().register(new Listener());
+        SwanEventBus.returnEventBus().register(new ErrorListener());
         db = new DatabasePool("test.db");
         poolHandler = new PoolHandler("test.db");
         try(
@@ -38,7 +38,7 @@ public class PoolHandlerTest {
             stat.execute("DELETE FROM parameters WHERE 1=1;");
         }
         catch(SQLException e){
-            System.out.println(e);
+            System.out.println(e.toString());
         }
     }
 
@@ -58,7 +58,7 @@ public class PoolHandlerTest {
     }
 
     @Test
-    void insertToDatabase(){
+    void insertToDatabaseTest(){
 
 
         try(
@@ -73,25 +73,42 @@ public class PoolHandlerTest {
             assertEquals("test-params", parameter.getParameters(), "params not inserted into database");
         }
         catch(SQLException e){
-            fail(e);
+            fail(e.toString());
         }
     }
 
     @Test
-    void selectsFromDatabase(){
+    void insertToDatabaseErrorTest(){
         try(
                 Connection conn = db.connection();
                 Statement stat = conn.createStatement();
         )
         {
-            stat.executeQuery("INSERT INTO parameters (name, params) VALUES ('test-name', 'test-params');");
+            poolHandler.insert("t-nametest-nametest-namenametest-nametest-nametest-nametnametest-nam","test-params");
+            ResultSet results = stat.executeQuery("SELECT name, params FROM parameters;");
+
+            assertNotNull(error);
+        }
+        catch(SQLException e){
+            fail(e.toString());
+        }
+    }
+
+    @Test
+    void selectsFromDatabaseTest(){
+        try(
+                Connection conn = db.connection();
+                Statement stat = conn.createStatement();
+        )
+        {
+            stat.execute("INSERT INTO parameters (name, params) VALUES ('test-name', 'test-params');");
             poolHandler.select();
             assertEquals("test-name", name, "name not selected from database");
             assertEquals("test-params", params, "params not selected from database");
 
         }
         catch(SQLException e){
-            fail(e);
+            fail(e.toString());
         }
     }
 
