799006b33c819f2781f3744090691186103bf5cc
Martin Cameron <highlandrocketofficial@gmail.com>
2026-03-06 04:28:27 +0000

    added missing

--- src/main/java/org/example/cob/customevents/InsertEvent.java
+++ src/main/java/org/example/cob/customevents/InsertEvent.java
@@ -1,5 +1,20 @@
 package org.example.cob.customevents;
 
 public class InsertEvent {
+    String name;
+    String parameters;
+
+    public InsertEvent(String name, String parameters){
+        this.name = name;
+        this.parameters = parameters;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getParameters(){
+        return parameters;
+    }
 
 }
