4c914c84f8ac2ec53908b6af46ca33c4d5f1c8ca
Martin Cameron <highlandrocketofficial@gmail.com>
2026-04-14 06:23:44 +0100

    updated logic and test for new inputToken functionality

--- src/test/java/org/example/cob/parser/InputTokenTest.java
+++ src/test/java/org/example/cob/parser/InputTokenTest.java
@@ -1,24 +1,58 @@
 package org.example.cob.parser;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InputTokenTest {
 
     InputToken inputToken;
 
     @BeforeEach
-    public void instatiate(){
-        inputToken = new InputToken("NAME", "TYPE");
+    public void instantiate(){
+        inputToken = new InputToken(
+                "TYPE",
+                "NAME",
+                "LABEL",
+                0,
+                1
+        );
     }
 
     @Test
-    void inputTokenHasTypeTest(){
+    void inputTokenTypeTest(){
+
         String type = inputToken.getType();
+        assertEquals("TYPE", type);
     }
 
     @Test
-    void inputTokenHasNameTest(){
+    void inputTokenNameTest(){
+
         String name = inputToken.getName();
+        assertEquals("NAME", name);
+    }
+
+    @Test
+    void inputTokenLabelTest(){
+
+        String label = inputToken.getLabel();
+        assertEquals("LABEL", label);
+    }
+
+    @Test
+    void inputTokenMinTest(){
+
+        int min = inputToken.getMin();
+        assertEquals(0, min);
+    }
+
+    @Test
+    void inputTokenMaxTest(){
+
+        int max = inputToken.getMax();
+        assertEquals(1, max);
     }
 }
