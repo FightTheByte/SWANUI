c1a17f1810c32053fdb40bbd6675592ab5d3de1f
Martin Cameron <highlandrocketofficial@gmail.com>
2026-04-14 09:38:29 +0100

    finished FormGeneratorTest

--- src/test/java/org/example/cob/UI/FormGeneratorTest.java
+++ src/test/java/org/example/cob/UI/FormGeneratorTest.java
@@ -1,4 +1,36 @@
 package org.example.cob.UI;
+import java.util.List;
+import com.dlsc.formsfx.model.structure.Section;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class FormGeneratorTest {
+
+    FormGenerator formGenerator;
+
+    @BeforeEach
+    public void instantiate(){
+        this.formGenerator = new FormGenerator("src/main/resources/swan-test.csv");
+    }
+
+    @Test
+    void isInstantiatedTest(){
+        assertTrue(formGenerator != null);
+    }
+
+    @Test
+    void returnsArrayOfSectionsTest(){
+        List<Section> sections = formGenerator.getSections();
+        assertEquals(2, sections.size());
+    }
+
+    @Test
+    void assignsTitlesToSections(){
+        List<Section> sections = formGenerator.getSections();
+        String title = sections.get(0).getTitle();
+        assertEquals("Start-up", title);
+    }
 }
