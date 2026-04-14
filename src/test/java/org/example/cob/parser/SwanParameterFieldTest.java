7522b3eb783aeeec8ac22a30cc1854eaf24978d0
Martin Cameron <highlandrocketofficial@gmail.com>
2026-04-14 07:09:08 +0100

    finished SwanParameterFieldTest

--- src/test/java/org/example/cob/parser/SwanParameterFieldTest.java
+++ src/test/java/org/example/cob/parser/SwanParameterFieldTest.java
@@ -1,4 +1,40 @@
 package org.example.cob.parser;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SwanParameterFieldTest {
+
+    SwanParameterField swanParameterField;
+
+    @BeforeEach
+    public void instantiate(){
+        this.swanParameterField = new SwanParameterField("src/main/resources/swan-test.csv");
+    }
+
+    @Test
+    void isInstantiated(){
+        assertTrue(swanParameterField instanceof SwanParameterField);
+    }
+
+    @Test
+    void createsInputTokensWithoutCrashingTest(){
+        try{
+            swanParameterField.createInputTokens();
+        } catch (Exception e) {
+            fail();
+        }
+    }
+
+    @Test
+    void getInputTokensTest(){
+        List<List<List<List<InputToken>>>> inputTokens = swanParameterField.getInputTokens();
+        InputToken inputToken = inputTokens.getFirst().getFirst().getFirst().getFirst();
+        int max = inputToken.getMax();
+        assertEquals(1, max);
+    }
 }
