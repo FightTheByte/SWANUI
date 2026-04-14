b6b892d00e5c7ad9fe44b149594b567f793a2be4
Martin Cameron <highlandrocketofficial@gmail.com>
2026-04-14 06:23:44 +0100

    updated logic and test for new inputToken functionality

--- src/main/java/org/example/cob/parser/InputToken.java
+++ src/main/java/org/example/cob/parser/InputToken.java
@@ -4,17 +4,35 @@ public class InputToken {
 
     String type;
     String name;
+    String label;
+    int max;
+    int min;
 
-    public InputToken(String name, String type) {
+    InputToken(
+            String type,
+            String name,
+            String label,
+            int min,
+            int max
+    ){
         this.type = type;
         this.name = name;
+        this.label = label;
+        this.min = min;
+        this.max = max;
     }
 
-    public String getType() {
+    String getName(){
+        return name;
+    }
+
+    String getType(){
         return type;
     }
 
-    public String getName() {
-        return name;
-    }
+    String getLabel(){return label;}
+
+    int getMin(){return min;}
+
+    int getMax(){return max;}
 }
