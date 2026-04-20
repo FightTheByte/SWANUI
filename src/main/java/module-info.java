ed1d1efc66e4a4fa9fee69d6f6946c3c5b9836d8
Martin Cameron <highlandrocketofficial@gmail.com>
2026-04-20 18:57:36 +0100

    finished UI and file logic

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,7 +7,7 @@ module org.example.cob {
     requires java.sql;
     requires java.json;
     requires com.opencsv;
-    opens org.example.cob to javafx.fxml;
+    opens org.example.cob to javafx.fxml, com.google.common;
     opens org.example.cob.eventbus to com.google.common;
     opens org.example.cob.handlers to com.google.common;
     exports org.example.cob;
--- src/main/java/org/example/cob/swan/SwanProcessBuilder.java
+++ src/main/java/org/example/cob/swan/SwanProcessBuilder.java
@@ -1,18 +1,15 @@
 package org.example.cob.swan;
 
-import java.io.IOException;
-import java.util.List;
-
 public class SwanProcessBuilder {
     ProcessBuilder processBuilder;
 
     public String runModel(String[] commands){
+
         try{
             processBuilder = new ProcessBuilder(commands);
             Process process = processBuilder.start();
-            process.waitFor();
-            String results = new String(process.getInputStream().readAllBytes());
-            return results;
+
+            return new String(process.getInputStream().readAllBytes());
         } catch(Exception e){
             return e.toString();
         }
